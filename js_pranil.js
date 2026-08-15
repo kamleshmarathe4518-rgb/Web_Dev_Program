3dc540c57eaddb95ca4eff0a74cd702315021570
undefined_pranil <sonawanepranil2006@gmail.com>
2026-08-15 23:27:51 +0530

    Please check  15th aug's work

--- js_pranil.js
+++ js_pranil.js
@@ -1,18 +1,1 @@
-// Basic JavaScript program
-
-console.log("Hello, World!");
-
-// Variables
-let name = "Pranil";
-let age = 19;
-
-// Display values
-console.log("Name:", name);
-console.log("Age:", age);
-
-// Function
-function greet(name) {
-    return "Hello, " + name + "!";
-}
-
-console.log(greet(name));
+alert("Hello, World!");
